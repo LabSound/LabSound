6356f129d296b9f4465370dc55bebcd6bf4aad8b
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2014-12-28 11:10:11 -0800

    fix misplaced endif

--- src/WTF/wtf/CurrentTime.cpp
+++ src/WTF/wtf/CurrentTime.cpp
@@ -200,15 +200,14 @@ double currentTime()
 }
 
 #endif // USE(QUERY_PERFORMANCE_COUNTER)
-
+#endif // PLATFORM(WINDOWS)
+    
 double currentTime()
 {
     struct timeval now;
     gettimeofday(&now, 0);
     return now.tv_sec + now.tv_usec / 1000000.0;
 }
-
-#endif
 
 #if PLATFORM(MAC)
 
