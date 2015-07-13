120bfce3181cd21d4dbcaaf234f39f75f64cf64a
Dimitri Diakopoulos <ddiakopoulos@gmail.com>
2015-07-13 15:32:11 -0700

    refactoring sampled instrument node a bit; still a couple lingering todos

--- include/LabSound/extended/SampledInstrumentNode.h
+++ include/LabSound/extended/SampledInstrumentNode.h
@@ -18,163 +18,30 @@
 #include <string>
 #include <algorithm>
 
-namespace LabSound {
+namespace LabSound 
+{
+	class SamplerSound;
 
-	using namespace WebCore;
-
-	struct SamplerSound {
-
-		SamplerSound(
-            std::shared_ptr<GainNode> destination,
-			std::string path, 
-			std::string baseMidiNote, 
-			std::string midiNoteLow, 
-			std::string midiNoteHigh,
-            float sampleRate) {
-
-			audioBuffer = new SoundBuffer(path.c_str(), sampleRate);
-
-			this->baseMidiNote = getMIDIFromNoteString(baseMidiNote);
-			this->midiNoteLow = getMIDIFromNoteString(midiNoteLow);
-			this->midiNoteHigh = getMIDIFromNoteString(midiNoteHigh);
-
-			this->destinationNode = destination;
-
-		}
-
-		bool appliesToNote(uint8_t note) {
-
-			//std::cout << "Note: " << int(note) << std::endl;
-			//std::cout << "Base: " << int(baseMidiNote) << std::endl;
-			//std::cout << "Low: " << int(midiNoteLow) << std::endl;
-			//std::cout << "High: " << int(midiNoteHigh) << std::endl;
-			//std::cout << std::endl << std::endl;
-
-			if (baseMidiNote == note) {
-				return true; 
-			} else if (note >= midiNoteLow && note <= midiNoteHigh) {
-				return true; 
-			}
-
-			else return false; 
-
-		}
-
-		std::shared_ptr<AudioBufferSourceNode> startNote(ContextRenderLock& r, uint8_t midiNoteNumber, float amplitude = 1.0) {
-            
-            auto ac = r.context();
-            if (!ac) return nullptr;
-            
-			// var semitoneRatio = Math.pow(2, 1/12);
-			double pitchRatio = pow(2.0, (midiNoteNumber - baseMidiNote) / 12.0);
-
-			std::shared_ptr<AudioBufferSourceNode> theSample(audioBuffer->create(r, ac->sampleRate()));
-
-			theSample->playbackRate()->setValue(pitchRatio); 
-			theSample->gain()->setValue(amplitude); 
-
-			// Connect the source node to the parsed audio data for playback
-			theSample->setBuffer(r, audioBuffer->audioBuffer);
-
-			theSample->connect(ac, destinationNode.get(), 0, 0);
-			theSample->start(0.0);
-
-			return theSample;
-
-		}
-
-		// Ex: F#6. Assumes uppercase note names, hash symbol, and octave. 
-		uint8_t getMIDIFromNoteString(std::string noteName) {
-
-			std::array<std::string, 12> midiTranslationArray = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-
-			// Ocatve is always last character, as an integer 
-			std::string octaveString = noteName.substr(noteName.length() - 1, 1);
-			int octave = std::stoi(octaveString);
-
-			std::string noteString = noteName.erase(noteName.length() - 1, 1); 
-
-			// Uppercase the incoming note 
-			std::transform(noteString.begin(), noteString.end(), noteString.begin(), ::toupper);
-
-			// IF we don't use # notation, convert S to #
-			std::replace(noteString.begin(), noteString.end(), 'S', '#'); // replace all 'x' to 'y'
-
-			// Note name is now the first or second character 
-			int notePos = -1;
-			for (int i = 0; i < 12; ++i) {
-				if (noteString == midiTranslationArray[i]) {
-					notePos = i;
-					break;
-				}
-			}
-
-			return uint8_t((octave * 12.0) + notePos);
-
-		}
-
-		std::string getNoteStringFromMIDI(uint8_t note) {
-
-			std::array<std::string, 12> midiTranslationArray = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
-
-			int octave = int(note / 12) - 1;
-			int positionInOctave = note % 12;
-
-			std::string originalNote = midiTranslationArray[positionInOctave];
-
-			std::replace(originalNote.begin(), originalNote.end(), '#', 'S'); // replace all 'x' to 'y'
-
-			return  originalNote + std::to_string(octave);
-
-		}
-
-		void stopNote(); 
-
-        std::shared_ptr<GainNode> destinationNode;
-
-		SoundBuffer *audioBuffer;
-
-		uint8_t baseMidiNote;
-		uint8_t midiNoteLow;
-		uint8_t midiNoteHigh; 
-
-	};
-
-	class SampledInstrumentNode {
-
+	// This class is a little but subversive of the typical LabSound node pattern. 
+	// Instead of inheriting from a node and injecting samples into an audio buffer,
+	// it internally creates ad-hoc AudioBufferSourceNode(s) and keeps them around
+	// in the graph so long as our gain node has been connected. 
+	class SampledInstrumentNode 
+	{
+		std::vector<std::shared_ptr<SamplerSound>> samples;
+		std::shared_ptr<WebCore::GainNode> gainNode;
 	public:
         SampledInstrumentNode(float sampleRate);
-        virtual ~SampledInstrumentNode() {}
-        
-		void loadInstrumentConfiguration(std::string path);
+        ~SampledInstrumentNode();
 
-		void noteOn(ContextRenderLock& r, float frequency, float amplitude);
-		float noteOff(ContextRenderLock& r, float amplitude);
+		void LoadInstrumentFromJSON(const std::vector<uint8_t> & jsonFile);
 
-		void stopAll(); 
+		void NoteOn(ContextRenderLock & r, const int midiNoteNumber, const float amplitude);
+		void NoteOff(ContextRenderLock & r, const int midiNoteNumber, const float amplitude);
 
-        std::shared_ptr<GainNode> gainNode;
+		void KillAllNotes(); 
 
-	private:
-
-		// void startVoice(SamplerVoice const &voice, SamplerSound const &sound, const uint8_t midiNoteNumber, const float velocity);
-		// void stopVoice(SamplerVoice const &voice);
-
-		// Sample Map => Audio Buffers
-
-		// Loop? 
-		// Filter? 
-
-		// Blech
-        std::vector<std::shared_ptr<SamplerSound>> samples;
-
-		// Satisfy the AudioNode interface
-		//virtual void process(ContextRenderLock&, size_t) override {}
-		//virtual void reset() override {}
-		//virtual double tailTime() const override { return 0; }
-		//virtual double latencyTime() const override { return 0; }
-		//virtual bool propagatesSilence(double now) const override { return true; }
-
+		WebCore::GainNode * GetGain() { return gainNode.get(); }
 	};
 
 } // LabSound
--- src/extended/SampledInstrumentNode.cpp
+++ src/extended/SampledInstrumentNode.cpp
@@ -1,4 +1,3 @@
-
 // Copyright (c) 2014 Dimitri Diakopolous, All rights reserved.
 // License is MIT: http://opensource.org/licenses/MIT
 
@@ -13,90 +12,190 @@
 #include <streambuf>
 #include <stdlib.h>
 
-#if OS(WINDOWS)
-	#include <direct.h>
-	#define getcwd _getcwd
-	#define MAX_PATH _MAX_PATH
-#endif
-
-#if OS(DARWIN)
-	#include <limits.h>
-	#include <unistd.h>
-	#define MAX_PATH PATH_MAX
-#endif
-
-namespace LabSound {
+namespace LabSound 
+{
 
 	using namespace json11;
+	using namespace WebCore;
 
-    SampledInstrumentNode::SampledInstrumentNode(float sampleRate) {
+	struct SamplerSound 
+	{
 
-		// All samples bus their output to this node... 
+		SamplerSound(std::shared_ptr<GainNode> d, std::string path, std::string root, std::string min, std::string max) : destinationNode(d)
+		{
+			audioBuffer = new SoundBuffer(path.c_str(), d->sampleRate());
+			this->baseMidiNote = MakeMIDINoteFromString(root);
+			this->midiNoteLow = MakeMIDINoteFromString(min);
+			this->midiNoteHigh = MakeMIDINoteFromString(max);
+		}
+
+		bool AppliesToNote(uint8_t note) 
+		{
+			// Debugging: 
+			//std::cout << "Note: " << int(note) << std::endl;
+			//std::cout << "Base: " << int(baseMidiNote) << std::endl;
+			//std::cout << "Low: " << int(midiNoteLow) << std::endl;
+			//std::cout << "High: " << int(midiNoteHigh) << std::endl;
+			//std::cout << std::endl << std::endl;
+			if (baseMidiNote == note || (note >= midiNoteLow && note <= midiNoteHigh)) 
+				return true; 
+			else 
+				return false; 
+		}
+
+		std::shared_ptr<AudioBufferSourceNode> Start(ContextRenderLock & r, int midiNoteNumber, float amplitude = 1.0) 
+		{
+            if (!r.context()) throw std::runtime_error("cannot get context");
+            
+			double pitchRatio = pow(2.0, (midiNoteNumber - baseMidiNote) / 12.0);
+
+			std::shared_ptr<AudioBufferSourceNode> theSample(audioBuffer->create(r, r.context()->sampleRate()));
+
+			theSample->playbackRate()->setValue(pitchRatio); 
+			theSample->gain()->setValue(amplitude); 
+
+			// Connect the source node to the parsed audio data for playback
+			theSample->setBuffer(r, audioBuffer->audioBuffer);
+
+			theSample->connect(r.context(), destinationNode.get(), 0, 0);
+			theSample->start(0.0);
+
+			return theSample;
+		}
+
+		void Stop(ContextRenderLock & r, int midiNoteNumber, float amplitude = 0.0)
+		{
+			//@tofix
+			// disconnect? 
+		}
+
+		// Ex: F#6. Assumes uppercase note names, hash symbol for sharp, and octave. 
+		uint8_t MakeMIDINoteFromString(std::string noteName) 
+		{
+			const std::array<std::string, 12> midiTranslationArray = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+			// Ocatve is always last character, as an integer 
+			std::string octaveString = noteName.substr(noteName.length() - 1, 1);
+			int octave = std::stoi(octaveString);
+
+			std::string noteString = noteName.erase(noteName.length() - 1, 1); 
+
+			std::transform(noteString.begin(), noteString.end(), noteString.begin(), ::toupper);
+
+			// IF we don't use # notation, convert S to #
+			std::replace(noteString.begin(), noteString.end(), 'S', '#');
+
+			// Note name is now the first or second character 
+			int notePos = -1;
+			for (int i = 0; i < 12; ++i) 
+			{
+				if (noteString == midiTranslationArray[i])
+				{
+					notePos = i;
+					break;
+				}
+			}
+
+			return uint8_t((octave * 12.0) + notePos);
+		}
+
+		std::string MakeStringFromMIDINote(uint8_t note)
+		{
+			const std::array<std::string, 12> midiTranslationArray = { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+
+			int octave = int(note / 12) - 1;
+			int positionInOctave = note % 12;
+
+			std::string originalNote = midiTranslationArray[positionInOctave];
+			std::replace(originalNote.begin(), originalNote.end(), '#', 'S');
+
+			return  (originalNote + std::to_string(octave));
+		}
+
+        std::shared_ptr<GainNode> destinationNode;
+
+		SoundBuffer * audioBuffer;
+
+		uint8_t baseMidiNote;
+		uint8_t midiNoteLow;
+		uint8_t midiNoteHigh; 
+	};
+
+    SampledInstrumentNode::SampledInstrumentNode(float sampleRate) 
+	{ 
         gainNode = std::make_shared<GainNode>(sampleRate);
-		gainNode->gain()->setValue(4.0);
-        //initialize(); note - not subclassed from AudioNode
+		gainNode->gain()->setValue(1.0);
 	}
 
-	// Definitely have ADSR... 
-	void SampledInstrumentNode::noteOn(ContextRenderLock& r, float midiNoteNumber, float amplitude)
+	SampledInstrumentNode::~SampledInstrumentNode()
+	{
+
+	}
+
+	//@tofix: add adsr
+	void SampledInstrumentNode::NoteOn(ContextRenderLock & r, const int midiNoteNumber, const float amplitude)
     {
-        auto ac = r.context();
-        if (!ac) return;
-        
-		for (auto &sample : samples)
+        if (!r.context()) return;
+		for (const auto & sample : samples)
         {
 			// Find note in sample map
-			if (sample->appliesToNote(midiNoteNumber))
+			if (sample->AppliesToNote(midiNoteNumber))
             {
-				sample->startNote(r, midiNoteNumber, amplitude);
+				sample->Start(r, midiNoteNumber, amplitude);
 			}
 		}
 	}
 
-	void SampledInstrumentNode::loadInstrumentConfiguration(std::string path) {
+	void SampledInstrumentNode::NoteOff(ContextRenderLock & r, const int midiNoteNumber, const float amplitude)
+	{
+		if (!r.context()) return;
+		for (const auto & sample : samples)
+        {
+			if (sample->AppliesToNote(midiNoteNumber))
+            {
+				sample->Stop(r, midiNoteNumber, amplitude);
+			}
+		}
+	}
 
-		char cwd[MAX_PATH];
-		getcwd(cwd, MAX_PATH);
+	void SampledInstrumentNode::LoadInstrumentFromJSON(const std::vector<uint8_t> & jsonFile) 
+	{
+		std::ifstream fileStream((char*)jsonFile.data());
 
-		std::string swdString(cwd);
-
-		std::ifstream fileStream(path);
-		std::string jsonString;
-        
-        if ( !fileStream.is_open() ) {
-            LOG_ERROR("Instrument JSON failed to open with path %s: ", path.c_str());
+        if (!fileStream.is_open()) 
+		{
+            LOG_ERROR("JSON file failed to open");
             return;
         }
         
+		std::string jsonString;
 		fileStream.seekg(0, std::ios::end);
 		jsonString.reserve(fileStream.tellg());
 		fileStream.seekg(0, std::ios::beg);
 
 		jsonString.assign((std::istreambuf_iterator<char>(fileStream)), std::istreambuf_iterator<char>());
 
-		std::string err;
-		auto jsonConfig = Json::parse(jsonString, err);
+		std::string jsonParseErr;
+		auto jsonConfig = Json::parse(jsonString, jsonParseErr);
 
-		if (err.empty()) {
+		if (jsonParseErr.empty()) 
+		{
+			for (const auto & samp : jsonConfig["samples"].array_items()) 
+			{
+				auto path = samp["sample"].string_value();
+				auto root = samp["baseNote"].string_value();
+				auto min = samp["lowNote"].string_value();
+				auto max = samp["highNote"].string_value();
 
-			for (auto &samp : jsonConfig["samples"].array_items()) {
 				// std::cout << "Loading Sample: " << samp.dump() << "\n";
 				// std::cout << "Sample Name: " << samp["sample"].string_value() << std::endl;
-                samples.emplace_back(std::make_shared<SamplerSound>(
-					gainNode,
-					samp["sample"].string_value(),
-					samp["baseNote"].string_value(),
-					samp["lowNote"].string_value(),
-					samp["highNote"].string_value(),
-                    44100
-					));
+                samples.emplace_back(std::make_shared<SamplerSound>(gainNode, path, root, min, max));
 			}
-
 		}
 		
 		else 
 		{
-			LOG_ERROR("JSON Parse Error: %s", err.c_str());
+			LOG_ERROR("JSON Parse Error: %s", jsonParseErr.c_str());
 		}
 
 	}
