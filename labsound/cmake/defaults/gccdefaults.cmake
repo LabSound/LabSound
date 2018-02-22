68c23db438a706beefb9fa5b3cdc9a57c8de3ec3
Avaer Kazmer <a@modules.io>
2018-02-22 10:01:31 -0500

    More cmake files cleanup

--- labsound/cmake/defaults/gccdefaults.cmake
+++ labsound/cmake/defaults/gccdefaults.cmake
@@ -1,5 +1,1 @@
-
-# This file contains a set of flags/settings shared between our 
-# GCC and Clang configs. This allows clangdefaults and gccdefaults
-# to remain minimal, marking the points where divergence is required.
-
+# nothing
