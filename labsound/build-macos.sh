7703e6b499a162aeee1edb91e253c67430586508
Avaer Kazmer <a@modules.io>
2018-02-23 01:15:20 -0500

    Bugfix macos build script to include libnyquist

--- labsound/build-macos.sh
+++ labsound/build-macos.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 xcodebuild -project osx/LabSound.xcodeproj -target LabSound -configuration Release
+xcodebuild -project third_party/libnyquist/libnyquist.xcodeproj -target libnyquist -configuration Release
