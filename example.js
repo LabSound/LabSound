5b9387d993f1b12b4f41930c250926f793e5f10a
Avaer Kazmer <a@modules.io>
2018-02-23 06:35:17 -0500

    Add connection testing to example.js

--- example.js
+++ example.js
@@ -1,18 +1,29 @@
 const path = require('path');
 const fs = require('fs');
-const WebAudio = require('.');
-console.log('got web audio', WebAudio);
-const {Audio} = WebAudio;
+const {AudioContext, Audio} = require('.');
+
+const audioContext = new AudioContext();
 
 fs.readFile(path.join(__dirname, 'labsound', 'assets', 'samples', 'stereo-music-clip.wav'), (err, data) => {
   if (!err) {
     console.log('create');
+
     const audio = new Audio();
+
     console.log('load');
+
     audio.load(data, 'wav');
+    
+    const audioSourceNode = audioContext.createMediaElementSource(audio);
+    const audioDestinationNode = audioContext.destination;
+    audioSourceNode.connect(audioDestinationNode, 0, 0);
+
     console.log('play');
+
     audio.play();
+
     console.log('set timeout');
+
     setTimeout(() => {
       audio.pause();
 
