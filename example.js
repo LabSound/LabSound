cbba4a2f73aab4feb79c993cbacef0fe85d14604
Avaer Kazmer <a@modules.io>
2018-02-23 02:36:31 -0500

    Clean up example.js

---
+++ example.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+const WebAudio = require('.');
+console.log('got web audio', WebAudio);
+const {HTMLAudioElement} = WebAudio;
+
+fs.readFile(path.join(__dirname, 'labsound', 'assets', 'samples', 'stereo-music-clip.wav'), (err, data) => {
+  if (!err) {
+    console.log('create');
+    const audioNode = new HTMLAudioElement();
+    console.log('load');
+    audioNode.load(data, 'wav');
+    console.log('play');
+    audioNode.play();
+    console.log('set timeout');
+    setTimeout(() => {
+      audioNode.pause();
+
+      setTimeout(() => {}, 1000);
+    }, 2000);
+  } else {
+    throw err;
+  }
+});
