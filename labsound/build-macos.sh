a2382582e2b41a5cb8ce9b3ddb86c68cd09742ce
Avaer Kazmer <a@modules.io>
2018-03-02 02:19:43 -0800

    Add xcodebuild to macos build script

--- labsound/build-macos.sh
+++ labsound/build-macos.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+xcodebuild -project osx/LabSound.xcodeproj -target LabSound -configuration Release clean
 xcodebuild -project osx/LabSound.xcodeproj -target LabSound -configuration Release
+
+xcodebuild -project third_party/libnyquist/libnyquist.xcodeproj -target libnyquist -configuration Release clean
 xcodebuild -project third_party/libnyquist/libnyquist.xcodeproj -target libnyquist -configuration Release
