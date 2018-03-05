5abcdf3fdce658a0445eb05bebcffe764257ba46
Avaer Kazmer <a@modules.io>
2018-03-05 12:23:30 -0500

    Remove dead macos build script libnyquist dependency

--- labsound/build-macos.sh
+++ labsound/build-macos.sh
@@ -6,6 +6,3 @@ popd
 
 xcodebuild -project osx/LabSound.xcodeproj -target LabSound -configuration Release clean
 xcodebuild -project osx/LabSound.xcodeproj -target LabSound -configuration Release
-
-xcodebuild -project third_party/libnyquist/libnyquist.xcodeproj -target libnyquist -configuration Release clean
-xcodebuild -project third_party/libnyquist/libnyquist.xcodeproj -target libnyquist -configuration Release
