502369b9da866dd1e8b840f161354a6cefb25ed7
Avaer Kazmer <a@modules.io>
2018-03-05 06:20:14 -0800

    Small index.js cleanup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const bindings = require('./build/Release/webaudio.node');
+// const bindings = require('./build/Debug/webaudio.node');
 
 const {PannerNode} = bindings;
 PannerNode.setPath(path.join(__dirname, 'labsound', 'assets', 'hrtf'));
