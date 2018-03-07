c847374cb59d342915c10754eac64fa8b6d2b1bf
Avaer Kazmer <a@modules.io>
2018-03-06 22:23:38 -0800

    Bugfix AudioFileReader impulse file reading

--- labsound/src/extended/AudioFileReader.cpp
+++ labsound/src/extended/AudioFileReader.cpp
@@ -311,9 +311,11 @@ std::mutex g_fileIOMutex;
 std::unique_ptr<AudioBus> MakeBusFromFile(const char *filePath, bool mixToMono, std::string *error)
 {
     std::lock_guard<std::mutex> lock(g_fileIOMutex);
-    std::ifstream inputStream(filePath);
-    std::istream_iterator<unsigned char> start(inputStream), end;
-    std::vector<unsigned char> buffer(start, end);
+    std::ifstream inputStream(filePath, std::ios::binary|std::ios::ate);
+    std::ifstream::pos_type size = inputStream.tellg();
+    std::vector<unsigned char> buffer(size);
+    inputStream.seekg(0, std::ios::beg);
+    inputStream.read((char *)buffer.data(), size);
     return detail::LoadInternal(buffer, mixToMono, error);
 }
 
