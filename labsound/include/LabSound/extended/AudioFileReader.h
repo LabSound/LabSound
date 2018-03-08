c528f269b0f10eca65ceb4f39b9ed0e9ed938c7b
Avaer Kazmer <a@modules.io>
2018-03-08 12:50:19 -0800

    More AudioFileReader cleanup

--- labsound/include/LabSound/extended/AudioFileReader.h
+++ labsound/include/LabSound/extended/AudioFileReader.h
@@ -23,37 +23,37 @@ extern "C" {
 
 #define SWR_CH_MAX 64
 
+// Allocator adaptor that interposes construct() calls to
+// convert value initialization into default initialization.
+template <typename T, typename A=std::allocator<T>>
+class default_init_allocator : public A {
+  typedef std::allocator_traits<A> a_t;
+public:
+  template <typename U> struct rebind {
+    using other =
+      default_init_allocator<
+        U, typename a_t::template rebind_alloc<U>
+      >;
+  };
+
+  using A::A;
+
+  template <typename U>
+  void construct(U* ptr)
+    noexcept(std::is_nothrow_default_constructible<U>::value) {
+    ::new(static_cast<void*>(ptr)) U;
+  }
+  template <typename U, typename...Args>
+  void construct(U* ptr, Args&&... args) {
+    a_t::construct(static_cast<A&>(*this),
+                   ptr, std::forward<Args>(args)...);
+  }
+};
+
 typedef std::vector<float, default_init_allocator<float>> Plane;
 typedef std::vector<Plane> PlanesVector;
 
 namespace detail {
-  // Allocator adaptor that interposes construct() calls to
-  // convert value initialization into default initialization.
-  template <typename T, typename A=std::allocator<T>>
-  class default_init_allocator : public A {
-    typedef std::allocator_traits<A> a_t;
-  public:
-    template <typename U> struct rebind {
-      using other =
-        default_init_allocator<
-          U, typename a_t::template rebind_alloc<U>
-        >;
-    };
-
-    using A::A;
-
-    template <typename U>
-    void construct(U* ptr)
-      noexcept(std::is_nothrow_default_constructible<U>::value) {
-      ::new(static_cast<void*>(ptr)) U;
-    }
-    template <typename U, typename...Args>
-    void construct(U* ptr, Args&&... args) {
-      a_t::construct(static_cast<A&>(*this),
-                     ptr, std::forward<Args>(args)...);
-    }
-  };
-
   class AppData {
   public:
     AppData();
