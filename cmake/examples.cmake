7a5b0081467260cfb6e95aace6dd4276606cad04
Nick Porcino <meshula@hotmail.com>
2018-09-02 22:45:16 -0700

    rename example to LabSoundExample

--- cmake/examples.cmake
+++ cmake/examples.cmake
@@ -3,10 +3,10 @@ project(LabSoundExamples)
 
 set(src "${LABSOUND_ROOT}/examples/src/ExamplesMain.cpp")
 
-add_executable(example ${src})
+add_executable(LabSoundExample ${src})
 
-set_cxx_version(example)
-_set_compile_options(example)
+set_cxx_version(LabSoundExample)
+_set_compile_options(LabSoundExample)
 
 
 if (APPLE)
@@ -18,11 +18,11 @@ if (APPLE)
         "-framework Cocoa")
 ENDIF (APPLE)
 
-target_link_libraries(example LabSound ${DARWIN_LIBS})
+target_link_libraries(LabSoundExample LabSound ${DARWIN_LIBS})
 
-set_target_properties(example PROPERTIES
+set_target_properties(LabSoundExample PROPERTIES
                       RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/bin)
 
-set_property(TARGET example PROPERTY FOLDER "examples")
+set_property(TARGET LabSoundExample PROPERTY FOLDER "examples")
 
-install (TARGETS example RUNTIME DESTINATION ${PROJECT_BINARY_DIR}/bin)
+install (TARGETS LabSoundExample RUNTIME DESTINATION ${PROJECT_BINARY_DIR}/bin)
