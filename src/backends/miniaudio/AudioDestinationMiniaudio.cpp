0dc776fcb81e4a0c8bc08e720371f150aecfa1de
Nick Porcino <meshula@hotmail.com>
2020-01-09 00:11:08 -0800

    Ring buffer to support miniaudio vs fixed render quantum

--- src/backends/miniaudio/AudioDestinationMiniaudio.cpp
+++ src/backends/miniaudio/AudioDestinationMiniaudio.cpp
@@ -4,8 +4,8 @@
 #include "AudioDestinationMiniaudio.h"
 #include "internal/VectorMath.h"
 
-#include "LabSound/core/AudioNode.h"
 #include "LabSound/core/AudioIOCallback.h"
+#include "LabSound/core/AudioNode.h"
 #include "LabSound/extended/Logging.h"
 
 #define MINIAUDIO_IMPLEMENTATION
@@ -38,45 +38,43 @@ AudioDestinationMiniaudio::AudioDestinationMiniaudio(AudioIOCallback & callback,
                                                      uint32_t numInputChannels,
                                                      uint32_t numChannels,
                                                      float sampleRate)
-: m_callback(callback)
-, m_numChannels(numChannels)
-, m_numInputChannels(numInputChannels)
-, m_sampleRate(sampleRate)
+: _callback(callback)
+, _numChannels(numChannels)
+, _numInputChannels(numInputChannels)
+, _sampleRate(sampleRate)
 {
     configure();
 }
 
 AudioDestinationMiniaudio::~AudioDestinationMiniaudio()
 {
-    ma_device_uninit(&device);
-    delete m_renderBus;
-    delete m_inputBus;
+    ma_device_uninit(&_device);
+    delete _renderBus;
+    delete _inputBus;
 }
 
 namespace
 {
-
-    void outputCallback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
+    void outputCallback(ma_device * pDevice, void * pOutput, const void * pInput, ma_uint32 frameCount)
     {
         // Buffer is nBufferFrames * channels, interleaved
         float * fBufOut = (float *) pOutput;
         AudioDestinationMiniaudio * audioDestination = reinterpret_cast<AudioDestinationMiniaudio *>(pDevice->pUserData);
         memset(fBufOut, 0, sizeof(float) * frameCount * audioDestination->channelCount());
-        audioDestination->render(frameCount, pOutput, const_cast<void*>(pInput));
+        audioDestination->render(frameCount, pOutput, const_cast<void *>(pInput));
     }
-
 }
 
 void AudioDestinationMiniaudio::configure()
 {
     ma_device_config deviceConfig = ma_device_config_init(ma_device_type_playback);
     deviceConfig.playback.format = ma_format_f32;
-    deviceConfig.playback.channels = m_numChannels;
-    deviceConfig.sampleRate = static_cast<uint32_t>(m_sampleRate);
+    deviceConfig.playback.channels = _numChannels;
+    deviceConfig.sampleRate = static_cast<uint32_t>(_sampleRate);
     deviceConfig.dataCallback = outputCallback;
     deviceConfig.performanceProfile = ma_performance_profile_low_latency;
     deviceConfig.pUserData = this;
-    if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS)
+    if (ma_device_init(NULL, &deviceConfig, &_device) != MA_SUCCESS)
     {
         LOG_ERROR("Unable to open audio playback device");
         return;
@@ -85,7 +83,7 @@ void AudioDestinationMiniaudio::configure()
 
 void AudioDestinationMiniaudio::start()
 {
-    if (ma_device_start(&device) != MA_SUCCESS)
+    if (ma_device_start(&_device) != MA_SUCCESS)
     {
         LOG_ERROR("Unable to start audio device");
         return;
@@ -94,61 +92,60 @@ void AudioDestinationMiniaudio::start()
 
 void AudioDestinationMiniaudio::stop()
 {
-    if (ma_device_stop(&device) != MA_SUCCESS)
+    if (ma_device_stop(&_device) != MA_SUCCESS)
     {
         LOG_ERROR("Unable to stop audio device");
         return;
     }
 }
 
-
 const float kLowThreshold = -1.0f;
 const float kHighThreshold = 1.0f;
+const int kRenderQuantum = 128;
 
 // Pulls on our provider to get rendered audio stream.
 void AudioDestinationMiniaudio::render(int numberOfFrames, void * outputBuffer, void * inputBuffer)
 {
-    if (!m_renderBus || m_renderBus->length() < numberOfFrames)
+    if (!_renderBus)
     {
-        if (m_renderBus)
-            delete m_renderBus;
-
-        m_renderBus = new AudioBus(m_numChannels, numberOfFrames);
-        m_renderBus->setSampleRate(m_sampleRate);
+        _renderBus = new AudioBus(_numChannels, kRenderQuantum, true);
+        _renderBus->setSampleRate(_sampleRate);
     }
 
-    float *myInputBufferOfFloats = (float*) inputBuffer;
-
-#if 0
-    if (m_inputBus)
+    if (!_inputBus && _numInputChannels)
     {
-        /// @TODO all the code for input!!!
+        _inputBus = new AudioBus(_numInputChannels, kRenderQuantum, true);
+        _inputBus->setSampleRate(_sampleRate);
     }
-#endif
 
-    // Source Bus :: Destination Bus
-    m_callback.render(m_inputBus, m_renderBus, numberOfFrames);
+    float * myInputBufferOfFloats = (float *) inputBuffer;
+    float * pOut = static_cast<float *>(outputBuffer);
+    size_t c = _renderBus->numberOfChannels();
 
-    // miniaudio expects interleaved data
-    float* pOut = static_cast<float*>(outputBuffer);
-    size_t c = m_renderBus->numberOfChannels();
-    for (size_t i = 0; i < c; ++i)
+    while (numberOfFrames > 0)
     {
-        const float* data = m_renderBus->channel(i)->data();
-        for (int j = 0; j < numberOfFrames; ++j)
-            pOut[i * c + j] = data[j];
+        if (_remainder > 0)
+        {
+            // use up samples from previous callback
+            int samples = _remainder < numberOfFrames ? _remainder : numberOfFrames;
+            for (int i = 0; i < c; ++i)
+            {
+                AudioChannel * channel = _renderBus->channel(i);
+                VectorMath::vclip(channel->data() + static_cast<ptrdiff_t>(kRenderQuantum - _remainder), 1, 
+                                  &kLowThreshold, &kHighThreshold, 
+                                  pOut + i, _numChannels, samples);
+            }
+            pOut += (c * samples);
+            numberOfFrames -= samples;
+            _remainder -= samples;
+        }
+        else
+        {
+            // generate new data
+            _callback.render(_inputBus, _renderBus, kRenderQuantum);
+            _remainder = kRenderQuantum;
+        }
     }
-#if 0
-    // miniaudio does its own clipping
-
-    // Clamp values at 0db (i.e., [-1.0, 1.0])
-    for (unsigned i = 0; i < m_renderBus->numberOfChannels(); ++i)
-    {
-        AudioChannel * channel = m_renderBus->channel(i);
-        VectorMath::vclip(channel->data(), 1, &kLowThreshold, &kHighThreshold, channel->mutableData(), 1, numberOfFrames);
-    }
-#endif
 }
 
-
-} // namespace lab
+}  // namespace lab
