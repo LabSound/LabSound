4e95c6b911f90480dda2761f9d3a4a0d08b4dcbb
Nick Porcino <meshula@hotmail.com>
2020-01-26 22:47:07 -0800

    Fix mac build

--- src/core/ConvolverNode.cpp
+++ src/core/ConvolverNode.cpp
@@ -56,7 +56,7 @@ static float calculateNormalizationScale(AudioBus * response)
     power = sqrt(power / (numberOfChannels * length));
 
     // Protect against accidental overload
-    if (std::isinf(power) || std::isnan(power) || power < MinPower)
+    if (isinf(power) || isnan(power) || power < MinPower)
         power = MinPower;
 
     float scale = 1 / power;
