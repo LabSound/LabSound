587697a8063f15ba380adf20ceb9c22fcda4da4a
dimitri <ddiakopoulos@gmail.com>
2020-02-09 09:16:43 -0800

    clean examples main of the refactoring status dashboard

--- examples/src/ExamplesMain.cpp
+++ examples/src/ExamplesMain.cpp
@@ -15,23 +15,23 @@ static constexpr int iterations = 1;
 
 int main (int argc, char *argv[]) try
 {   
-                                                  // [refactoring status 2.06.2020]
-    ex_simple simple;                             // ok
-    ex_playback_events playback_events;           // ok
-    ex_offline_rendering offline_rendering;       // crash
-    ex_tremolo tremolo;                           // ok
-    ex_frequency_modulation frequency_mod;        // ok
-    ex_runtime_graph_update runtime_graph_update; // ok
-    ex_microphone_loopback microphone_loopback;   // ok
-    ex_microphone_reverb microphone_reverb;       // ok
-    ex_peak_compressor peak_compressor;           // ok
-    ex_stereo_panning stereo_panning;             // ok
-    ex_hrtf_spatialization hrtf_spatialization;   // ok
-    ex_convolution_reverb convolution_reverb;     // ok
-    ex_misc misc;                                 // silence
-    ex_dalek_filter dalek_filter;                 // ok
-    ex_redalert_synthesis redalert_synthesis;     // ok
-    ex_wavepot_dsp wavepot_dsp;                   // ok
+                                                 
+    ex_simple simple;                            
+    ex_playback_events playback_events;          
+    ex_offline_rendering offline_rendering;      
+    ex_tremolo tremolo;                          
+    ex_frequency_modulation frequency_mod;       
+    ex_runtime_graph_update runtime_graph_update;
+    ex_microphone_loopback microphone_loopback;  
+    ex_microphone_reverb microphone_reverb;      
+    ex_peak_compressor peak_compressor;          
+    ex_stereo_panning stereo_panning;            
+    ex_hrtf_spatialization hrtf_spatialization;  
+    ex_convolution_reverb convolution_reverb;    
+    ex_misc misc;                                
+    ex_dalek_filter dalek_filter;                
+    ex_redalert_synthesis redalert_synthesis;    
+    ex_wavepot_dsp wavepot_dsp;                  
 
     // We can optionally play for a number of iterations as a way of testing lifetime & memory issues.
     for (int i = 0; i < iterations; ++i)
