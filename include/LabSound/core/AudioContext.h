888a19f1bf7f93e2b9f1a5d86bf35bd1fb5360c8
dimitri <ddiakopoulos@gmail.com>
2020-02-27 07:47:51 -0800

    clang format only

--- include/LabSound/core/AudioContext.h
+++ include/LabSound/core/AudioContext.h
@@ -99,8 +99,7 @@ public:
 
 private:
 
-    // @TODO migrate most of the internal datastructures such as PendingConnection
-    // into Internals as there's no need to expose these at all.
+    // @TODO migrate most of the internal datastructures such as PendingConnection into Internals as there's no need to expose these at all.
     struct Internals;
     std::unique_ptr<Internals> m_internal;
 
--- include/LabSound/core/OscillatorNode.h
+++ include/LabSound/core/OscillatorNode.h
@@ -9,87 +9,81 @@
 #include "LabSound/core/Macros.h"
 #include "LabSound/core/WaveTable.h"
 
-namespace lab {
-
-class AudioBus;
-class AudioContext;
-class AudioSetting;
-
-// params: frequency, detune
-// settings: type
-//
-class OscillatorNode : public AudioScheduledSourceNode
+namespace lab
 {
-public:
+    class AudioBus;
+    class AudioContext;
+    class AudioSetting;
 
-    OscillatorNode(const float sampleRate = LABSOUND_DEFAULT_SAMPLERATE);
-    virtual ~OscillatorNode();
+    // params: frequency, detune
+    // settings: type
+    //
+    class OscillatorNode : public AudioScheduledSourceNode
+    {
+    public:
+        OscillatorNode(const float sampleRate);
+        virtual ~OscillatorNode();
 
-    // AudioNode
-    virtual void process(ContextRenderLock&, size_t framesToProcess) override;
-    virtual void reset(ContextRenderLock&) override;
+        virtual void process(ContextRenderLock &, size_t framesToProcess) override;
+        virtual void reset(ContextRenderLock &) override;
 
-    OscillatorType type() const;
-    void setType(OscillatorType type);
+        OscillatorType type() const;
+        void setType(OscillatorType type);
 
-    std::shared_ptr<AudioParam> amplitude() { return m_amplitude; }
-    std::shared_ptr<AudioParam> frequency() { return m_frequency; }
-    std::shared_ptr<AudioParam> detune() { return m_detune; }
-    std::shared_ptr<AudioParam> bias() { return m_bias; }
+        std::shared_ptr<AudioParam> amplitude() { return m_amplitude; }
+        std::shared_ptr<AudioParam> frequency() { return m_frequency; }
+        std::shared_ptr<AudioParam> detune() { return m_detune; }
+        std::shared_ptr<AudioParam> bias() { return m_bias; }
 
+    protected:
 
-protected:
-    void _setType(OscillatorType type);
+        void _setType(OscillatorType type);
 
-    float m_sampleRate;
+        float m_sampleRate;
 
-    double _lab_phase = 0; // new sine oscillator
+        double _lab_phase = 0;  // new sine oscillator
 
-    std::shared_ptr<AudioParam> m_amplitude; // default 1
-    std::shared_ptr<AudioParam> m_frequency; // hz
-    std::shared_ptr<AudioParam> m_bias;  // default 0
+        std::shared_ptr<AudioParam> m_amplitude;  // default 1
+        std::shared_ptr<AudioParam> m_frequency;  // hz
+        std::shared_ptr<AudioParam> m_bias;       // default 0
 
-    // Detune value (deviating from the frequency) in Cents.
-    std::shared_ptr<AudioParam> m_detune;
+        // Detune value (deviating from the frequency) in Cents.
+        std::shared_ptr<AudioParam> m_detune;
 
-    virtual double tailTime(ContextRenderLock& r) const override { return 0; }
-    virtual double latencyTime(ContextRenderLock& r) const override { return 0; }
+        virtual double tailTime(ContextRenderLock & r) const override { return 0; }
+        virtual double latencyTime(ContextRenderLock & r) const override { return 0; }
 
-    void setWaveTable(std::shared_ptr<WaveTable> table);
+        void setWaveTable(std::shared_ptr<WaveTable> table);
 
-    // Returns true if there are sample-accurate timeline parameter changes.
-    bool calculateSampleAccuratePhaseIncrements(ContextRenderLock&, size_t framesToProcess);
+        // Returns true if there are sample-accurate timeline parameter changes.
+        bool calculateSampleAccuratePhaseIncrements(ContextRenderLock &, size_t framesToProcess);
 
-    virtual bool propagatesSilence(ContextRenderLock & r) const override;
+        virtual bool propagatesSilence(ContextRenderLock & r) const override;
 
-    void process_oscillator(ContextRenderLock & r, int frames);
+        void process_oscillator(ContextRenderLock & r, int frames);
 
-    // One of the waveform types defined in the enum.
-    std::shared_ptr<AudioSetting> m_type;
+        // One of the waveform types defined in the enum.
+        std::shared_ptr<AudioSetting> m_type;
 
-    // Frequency value in Hertz.
+        bool m_firstRender;
 
+        // m_virtualReadIndex is a sample-frame index into the buffer
+        double m_virtualReadIndex;
 
-    bool m_firstRender;
+        AudioFloatArray m_phaseIncrements;
+        AudioFloatArray m_biasValues;
+        AudioFloatArray m_detuneValues;
+        AudioFloatArray m_amplitudeValues;
 
-    // m_virtualReadIndex is a sample-frame index into our buffer representing the current playback position.
-    // Since it's floating-point, it has sub-sample accuracy.
-    double m_virtualReadIndex;
+        std::shared_ptr<WaveTable> m_waveTable;
 
-    AudioFloatArray m_phaseIncrements;
-    AudioFloatArray m_biasValues;
-    AudioFloatArray m_detuneValues;
-    AudioFloatArray m_amplitudeValues;
+        // Cache the wave tables for different waveform types, except CUSTOM.
+        static std::shared_ptr<WaveTable> s_waveTableSine;
+        static std::shared_ptr<WaveTable> s_waveTableSquare;
+        static std::shared_ptr<WaveTable> s_waveTableSawtooth;
+        static std::shared_ptr<WaveTable> s_waveTableTriangle;
+    };
 
-    std::shared_ptr<WaveTable> m_waveTable;
+}  // namespace lab
 
-    // Cache the wave tables for different waveform types, except CUSTOM.
-    static std::shared_ptr<WaveTable> s_waveTableSine;
-    static std::shared_ptr<WaveTable> s_waveTableSquare;
-    static std::shared_ptr<WaveTable> s_waveTableSawtooth;
-    static std::shared_ptr<WaveTable> s_waveTableTriangle;
-};
-
-} // namespace lab
-
-#endif // OscillatorNode_h
+#endif  // OscillatorNode_h
