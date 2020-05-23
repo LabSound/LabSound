860588caae9daeda10ad317025cfcf8bd68f40fb
Nick Porcino <meshula@hotmail.com>
2020-05-23 15:29:55 -0700

    Update C++ requirement to C++14

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -67,7 +67,7 @@ function(_set_cxx_14 proj)
     set(CMAKE_CXX_STANDARD 14)
     set(CXX_STANDARD_REQUIRED)
     if (APPLE)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -stdlib=libc++ -std=c++1y")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++14 -stdlib=libc++ -std=c++1y")
     endif()
 endfunction()
 
