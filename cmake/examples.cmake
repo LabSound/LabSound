77a58d0bdd16e90c79f01ced32ff1ec2a8432253
Philipp Dresselmann <philippdresselmann+github@gmail.com>
2023-09-11 09:58:50 +0200

    Fix MSVC compiler flags being passed to the MSYS toolchain
    Since MSYS uses GCC (or optionally clang), MSVC flags won't work here.

--- cmake/examples.cmake
+++ cmake/examples.cmake
@@ -11,10 +11,12 @@ set(CMAKE_CXX_STANDARD 14)
 set(proj LabSoundExample)
 
 if(WIN32)
-    # Arch AVX is problematic for many users, so disable it until
-    # some reasonable strategy (a separate AVX target?) is determined
-    #target_compile_options(${proj} PRIVATE /arch:AVX /Zi)
-    target_compile_options(${proj} PRIVATE /Zi)
+    if(MSVC)
+        # Arch AVX is problematic for many users, so disable it until
+        # some reasonable strategy (a separate AVX target?) is determined
+        #target_compile_options(${proj} PRIVATE /arch:AVX /Zi)
+        target_compile_options(${proj} PRIVATE /Zi)
+    endif(MSVC)
     target_compile_definitions(${proj} PRIVATE __WINDOWS_WASAPI__=1)
     # TODO: These vars are for libniquist and should be set in the find libynquist script.
     target_compile_definitions(${proj} PRIVATE HAVE_STDINT_H=1 HAVE_SINF=1)
